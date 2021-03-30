bd044f3d62fd665c55459748bfd8eea328fb8d4b
ShowBaba <samwise858@gmail.com>
2021-03-30 12:49:38 +0100

    setup for deployment

--- dist/dbConfig/db.js
+++ dist/dbConfig/db.js
@@ -6,15 +6,17 @@ require("reflect-metadata");
 const typeorm_1 = require("typeorm");
 const logger_1 = tslib_1.__importDefault(require("../utils/logger"));
 const User_1 = require("./entities/User");
+const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
+dotenv_1.default.config();
 function intializeDB() {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
         yield typeorm_1.createConnection({
             "type": "postgres",
-            "host": "localhost",
+            "host": "ec2-18-204-101-137.compute-1.amazonaws.com",
             "port": 5432,
-            "username": "postgres",
-            "password": "postgres",
-            "database": "leno-dev",
+            "username": "mubjxkeulkbrqj",
+            "password": "6e58789ba9ad48856b2356abd031bed4b8f5499e04e5d0a16573621b28bcb7c3",
+            "database": "d8hopie1mv50av",
             entities: [
                 User_1.User
             ],
--- src/dbConfig/db.ts
+++ src/dbConfig/db.ts
@@ -3,15 +3,17 @@ import { createConnection } from 'typeorm';
 // import { Tedis } from 'tedis';
 import logger from '../utils/logger';
 import { User } from './entities/User'
+import dotenv from 'dotenv';
+dotenv.config()
 
 export async function intializeDB(): Promise<void> {
   await createConnection({
     "type": "postgres",
-    "host": "localhost",
+    "host": "ec2-18-204-101-137.compute-1.amazonaws.com",
     "port": 5432,
-    "username": "postgres",
-    "password": "postgres",
-    "database": "leno-dev",
+    "username": "mubjxkeulkbrqj",
+    "password": "6e58789ba9ad48856b2356abd031bed4b8f5499e04e5d0a16573621b28bcb7c3",
+    "database": "d8hopie1mv50av",
     entities: [
       User
     ],
